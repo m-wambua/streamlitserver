963e175597b5bf18dbb34108e8f7734b74fa8b34
m-wambua <michaelmutisyawambua@gmail.com>
2024-02-18 08:42:33 +0300

    added the counts for catgorical variables and  did a description of the numerical variables together with histograms correlation maps and heat maps as well as a scatter matrix

--- front_end/app.py
+++ front_end/app.py
@@ -3,6 +3,9 @@ from InputOutput.file_reader import FileReader
 from InputOutput.fileextension import FileExtension
 from InputOutput.paramters import *
 from InputOutput.data_cleaner import DataCleaner
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Streamlit SessionState Class
 class SessionState:
@@ -19,20 +22,22 @@ class StreamlitApp:
     def run(self):
         with st.sidebar:
             st.title('Dexter Labs')
-            choice = st.radio('Navigation', ['Upload', 'Data Cleaning', 'Profiling', 'ML', 'Download'])
+            choice = st.radio('Navigation', ['Upload', 'Exploratory Data Analysis', 'Profiling', 'ML', 'Download'])
             st.info('This application allows you to build an automated ML pipeline ')
 
         if choice == 'Upload':
             self.upload_section()
 
-        elif choice == 'Data Cleaning':
+        elif choice == 'Exploratory Data Analysis':
             if session_state.df is not None:
-                self.data_cleaning_section()
+                self.exploratory_data_analysis_section(session_state.df)
             else:
                 st.warning('Please upload a file first.')
 
         elif choice == 'Profiling':
-            pass
+           pass
+            
+            
 
         elif choice == 'ML':
             pass
@@ -78,6 +83,9 @@ class StreamlitApp:
                         if session_state.df is not None:
                             st.dataframe(session_state.df)
                             st.success(f"File successfully loaded using {method} method.")
+                            if st.button('Next'):
+                                self.run()
+
                 else:
                     st.warning('Unable to determine the file extension.')
 
@@ -121,16 +129,127 @@ class StreamlitApp:
         else:
             return []
 
-    def data_cleaning_section(self):
-        st.title('Data Cleaning')
+    def exploratory_data_analysis_section(self, df):
+        st.title('Exploratory Data Analysis')
 
-        # Get user-selected data cleaning options
+        analyzer = DataAnalyzer(df)
+        analyzer.run()
+
+class DataAnalyzer:
+    
+    def __init__(self, df):
+        self.df = df
+
+    def run(self):
+        st.title('Data Analysis Options')
+        choice = st.radio('Select Analysis Type', ['Data Info', 'Data Cleaning'])  # Add more options as needed
+
+        if choice == 'Data Info':
+            self.data_info()
+        elif choice == 'Data Cleaning':
+            self.data_cleaning()
+
+    def data_info(self):
+        st.subheader('DataFrame Info')
+        info_output=self.get_info_output()
+        from io import StringIO
+        import pandas as pd
+        import matplotlib.pyplot as plt
+
+     
+
+
+        info_lines=info_output.split('\n')[3:-3]
+        info_str='\n'.join(info_lines)
+        info_file=StringIO(info_str)
+        info_df=pd.read_csv(info_file,sep='\s\s+',engine='python')
+    
+        st.table(info_df)
+        st.subheader('Categorical Columns')
+        categorical_cols=self.get_categorical_columns()
+        st.write(categorical_cols)
+        for col in categorical_cols:
+            st.subheader(f'Assessment of {col}')
+            counts=self.df[col].value_counts()
+            st.write(counts)
+
+
+        st.subheader('Numerical Columns')
+        numerical_cols=self.get_numerical_columns()
+        st.write(numerical_cols)
+        st.table(self.df.describe())
+
+        for col in numerical_cols:
+            st.subheader(f'Histogram of {col}')
+            self.plot_histogram(col)
+
+        self.correlation()
+        self.scatter_matrix()
+
+    def get_info_output(self):
+        import io
+        import sys
+        import pandas as pd
+        stdout=sys.stdout
+        sys.stdout=io.StringIO()
+
+        self.df.info()
+
+        info_output=sys.stdout.getvalue()
+
+        sys.stdout=stdout
+
+        return info_output
+    def get_categorical_columns(self):
+        import pandas as pd
+        categorical_cols=self.df.select_dtypes(include=['object']).columns.tolist()
+        return categorical_cols
+    
+    def get_numerical_columns(self):
+        import pandas as pd
+        numerical_cols=self.df.select_dtypes(include=['number']).columns.tolist()
+        return numerical_cols
+    
+    def plot_histogram(self,col):
+        fig, ax=plt.subplots()
+        ax.hist(self.df[col],bins=20,color='skyblue',edgecolor='black')
+        ax.set_xlabel(col)
+        ax.set_ylabel('Frequency')
+        ax.set_title(f'Histogram of {col}')
+        ax.grid(True)
+        st.pyplot(fig)
+    def correlation(self):
+        st.subheader('Correlation HeatMap')
+        numerical_column=self.df.select_dtypes(include=['float64','int64']).columns
+        selected_column=st.selectbox('Select Numerical Column', numerical_column)
+        st.write(f'Correlation Matrix for column: {selected_column}')
+        corr_matrix_plot=self.df[numerical_column].corr()
+        corr_matrix=self.df[numerical_column].corr()[selected_column].sort_values(ascending=False)
+        st.write(corr_matrix)
+        st.subheader('Correlation HeatMap')
+        fig,ax=plt.subplots()
+        sns.heatmap(corr_matrix_plot,annot=True,cmap='coolwarm',fmt='.2f',linewidths=0.5,ax=ax)
+        st.pyplot(fig)
+        return corr_matrix
+    
+    def scatter_matrix(self):
+        st.subheader('Scatter Matrix')
+        numerical_columns=self.df.select_dtypes(include=['float64','int64']).columns
+        selected_columns=st.multiselect('Select Numerical Columns', numerical_columns)
+        
+        if selected_columns:
+            scatter_matrix=sns.pairplot(self.df[selected_columns])
+            st.pyplot(scatter_matrix)
+
+    def data_cleaning(self):
+        st.subheader('Data Cleaning Section')
+        # Add data cleaning operations here
         cleaning_options = st.multiselect('Select Data Cleaning Options',
-                                         ['Handle Missing Values', 'Handle Duplicates', 'Correct Inconsistencies',
-                                          'Handle Outliers', 'Handle Inconsistent Data Types', 'Handle Irrelevant Data',
-                                          'Handle Inconsistent Formats', 'Handle Categorical Data', 'Scale and Normalize',
-                                          'Handle Skewness and Transformation', 'Handle Imbalanced Datasets',
-                                          'Data Integrity Checks', 'Data Quality Assessment', 'Iterative Process'])
+                                        ['Handle Missing Values', 'Handle Duplicates', 'Correct Inconsistencies',
+                                        'Handle Outliers', 'Handle Inconsistent Data Types', 'Handle Irrelevant Data',
+                                        'Handle Inconsistent Formats', 'Handle Categorical Data', 'Scale and Normalize',
+                                        'Handle Skewness and Transformation', 'Handle Imbalanced Datasets',
+                                        'Data Integrity Checks', 'Data Quality Assessment', 'Iterative Process'])
 
         data_cleaner = DataCleaner(session_state.df)
         # Apply selected data cleaning options
@@ -174,6 +293,7 @@ class StreamlitApp:
         # Display cleaned DataFrame
         st.dataframe(data_cleaner.df)
         st.success("Data cleaning completed successfully.")
+
 
 if __name__ == '__main__':
     app = StreamlitApp()
